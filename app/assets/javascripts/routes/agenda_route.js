924811b3d186944d8c184af16e33aea2e0df0e34
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-11-13 12:21:02 -0500

    Synchronization of removal of events from sidebar from the Agenda.

--- app/assets/javascripts/routes/agenda_route.js
+++ app/assets/javascripts/routes/agenda_route.js
@@ -5,8 +5,10 @@ App.AgendaRoute = Ember.Route.extend({
     console.log("In the model.");
     control = this.controllerFor("agenda");
     console.log(this.controllerFor("agenda").get('info.events'));
+    console.log(control);
     return {
-      events: Ember.A(this.controllerFor("agenda").get('info.events'))
+      events: Ember.A(this.controllerFor("agenda").get('info.events')),
+      sections_list: Ember.A(this.controllerFor("agenda").get('sections_list')),
     }
   },
   actions: {
