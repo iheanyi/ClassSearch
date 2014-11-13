59f39e474756c4e1426c813455227d84ad2fab1d
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-11-13 12:20:32 -0500

    Refactor Agenda into Calendar Component.

--- app/assets/javascripts/components/full-calendar_component.js
+++ app/assets/javascripts/components/full-calendar_component.js
@@ -3,8 +3,15 @@
 App.FullCalendarComponent = Ember.Component.extend({
   newEvent: "",
   eventTitle: "",
-
+  classNames: ['ag-component'],
+  events: [],
+  theEvents: [],
+  eventsChange: function() {
+    console.log("Events changed.");
+    this.rerender();
+  }.observes("theEvents"),
   _initializeCalendar: (function() {
+    console.log(this);
     return $("#calendar").fullCalendar({
       events: this.theEvents,
       weekends: false,
@@ -34,8 +41,33 @@ App.FullCalendarComponent = Ember.Component.extend({
       this.theEvents.pushObject(newEvent);
       this.$("#calendar").fullCalendar('renderEvent', newEvent, true);
     },
-    removeEvent: function() {
-      ag_events = this.theEvents;
+    removeSection: function(selected) {
+      console.log("Removing course!");
+      console.log("selected");
+      console.log(selected);
+      console.log(this);
+      this.sendAction('action', selected);
+
+      new_events = _.reject(this.theEvents, function(item) {
+        return item.section_id == selected.sectionID;
+      });
+
+      console.log("Events");
+      console.log(new_events);
+
+      new_sects = _.reject(this.sectionsList, function(item) {
+        return item.sectionID == selected.sectionID;
+      });
+
+      console.log("Sections");
+      console.log(new_sects);
+      this.theEvents = new_events;
+      this.sectionsList = new_sects;
+
+      console.log(this);
+      this.$("#calendar").fullCalendar('rerenderEvents');
+      //this.rerender();
+      //ag_events = this.theEvents;
     }
   }
 });
