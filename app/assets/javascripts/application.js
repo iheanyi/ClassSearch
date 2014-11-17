c9bebd77f05c92d0084af3f0b62e4798392403aa
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-11-17 18:09:14 -0500

    Remove Logging Information.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,9 +29,9 @@ $(document).foundation();
 // for more details see: http://emberjs.com/guides/application/
 App = Ember.Application.create({
   rootElement: '#ember-app',
-  LOG_TRANSITIONS: true,
-  LOG_TRANSITIONS_INTERNAL: true,
-  LOG_VIEW_LOOKUPS: true,
+  //LOG_TRANSITIONS: true,
+  //LOG_TRANSITIONS_INTERNAL: true,
+  //LOG_VIEW_LOOKUPS: true,
 });
 
 
