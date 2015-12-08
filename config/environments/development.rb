fecbee2c9f481bec2591fe03a372bbe749597e98
Iheanyi Ekechukwu <ekechukw@us.ibm.com>
2015-12-07 18:42:19 -0600

    Update dependencies, remove Growl usage.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
-    Bullet.growl = true
+    Bullet.growl = false
     Bullet.rails_logger = true
     Bullet.bugsnag = false
     Bullet.airbrake = false
